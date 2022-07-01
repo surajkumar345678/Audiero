bd7cfb990abff3faa3f724fdd94a66bb5530cb22
Suraj Kumar <kumarsuraj345678@gmail.com>
2022-07-02 02:00:07 +0530

    added songs

--- script.js
+++ script.js
@@ -1,25 +1,87 @@
-const music=document.querySelector('audio');
-const prevBtn=document.getElementById('prev');
-const playBtn=document.getElementById('play');
-const nextBtn=document.getElementById('next');
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven nation Army (Remix)',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row (Remix)',
+        artist: 'Jacinto Design',
+    }
+];
 
 // playing check
-let isPlaying=false;
+let isPlaying = false;
+
 // play
 function playSong() {
-    isPlaying=true;
-    playBtn.classList.replace('fa-play' , 'fa-pause');
-    playBtn.setAttribute('title','Pause');
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
     music.play();
 }
 
 // pause
 function pauseSong() {
-    isPlaying=false;
-    playBtn.classList.replace('fa-pause' , 'fa-play');
-    playBtn.setAttribute('title','Play');
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
     music.pause();
 }
 
 // play or pause 
-playBtn.addEventListener('click',()=>(isPlaying ? pauseSong() : playSong()));
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    title.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+let songIndex = 0;
+
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// on load select first song
+loadSong(songs[songIndex]);
+
+// event listener
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
